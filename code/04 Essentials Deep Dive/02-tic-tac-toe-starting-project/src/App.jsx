a930f09d6e280ed8c7ecd49d7222e50c5961b17f
MaryKaratu <Karatumary85@gmail.com>
2025-08-24 20:31:51 +0100

    Building Resuable Player Componnet

--- code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+import Player from "./Player";
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player playerName="Player 1" symbol="X" />
+          <Player playerName="Player 2" symbol="O" />
+        </ol>
+        Gameboard
+      </div>
+      Log
+    </main>
+  );
+}
 
 export default App;
