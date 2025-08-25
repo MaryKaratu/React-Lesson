280844b8bd63f80e1a6f65734cd8ea455b13454e
MaryKaratu <Karatumary85@gmail.com>
2025-08-25 13:46:50 +0100

    Use Input & Two-Way-Binding

--- code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/Player.jsx
+++ code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/Player.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
 export default function Player({ playerName, symbol }) {
+  const [prePlayerName, setPrevPlayerName] = useState(playerName);
   const [isEditing, setIsEditing] = useState(false);
+
+  function handlePlayerNameChange(e) {
+    setPrevPlayerName(e.target.value);
+  }
 
   function handleEdit() {
     setIsEditing((editing) => !editing);
   }
 
-  let PlayerNameEdit = <span className="player-name">{playerName}</span>;
+  let PlayerNameEdit = <span className="player-name">{prePlayerName}</span>;
 
   if (isEditing) {
-    PlayerNameEdit = <input type="text" required value={playerName} />;
+    PlayerNameEdit = (
+      <input
+        type="text"
+        required
+        value={prePlayerName}
+        onChange={handlePlayerNameChange}
+      />
+    );
   }
 
   return (
