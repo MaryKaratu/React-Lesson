65f95ed26413ed6fbf09a28e2e90f45dff26ed40
MaryKaratu <Karatumary85@gmail.com>
2025-08-27 15:03:12 +0100

    Disabling Buttons Conditionally

--- code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/GameBoard.jsx
+++ code/04 Essentials Deep Dive/02-tic-tac-toe-starting-project/src/GameBoard.jsx
@@ -36,7 +36,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((colSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={colSymbol !== null}
+                >
                   {colSymbol}
                 </button>
               </li>
