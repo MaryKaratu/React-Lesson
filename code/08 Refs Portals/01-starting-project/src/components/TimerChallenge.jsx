7bc8311346e9a7a73757863e34126319215ee720
MaryKaratu <Karatumary85@gmail.com>
2025-09-24 14:44:30 +0100

    Forwarding Refs to Custom Components

--- code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -3,12 +3,14 @@ import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dailog = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dailog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
@@ -20,7 +22,8 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      <ResultModal ref={dailog} targetTime={targetTime} result="lost" />
+
       <section className="challenge">
         <h2>{title}</h2>
 
