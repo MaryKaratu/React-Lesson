d8ef7b12437bc97fb5e9c6d5b46959a0e21ed8d7
MaryKaratu <Karatumary85@gmail.com>
2025-09-26 17:18:09 +0100

    Sharing State Across Componnet

--- code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ code/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -11,8 +11,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timerRemaining <= 0) {
     clearInterval(timer.current);
-    setTimerRemaining(targetTime * 1000);
     dailog.current.open();
+  }
+
+  function hnadleReset() {
+    setTimerRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -28,7 +31,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dailog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dailog}
+        targetTime={targetTime}
+        remainingTime={timerRemaining}
+        onReset={hnadleReset}
+      />
 
       <section className="challenge">
         <h2>{title}</h2>
