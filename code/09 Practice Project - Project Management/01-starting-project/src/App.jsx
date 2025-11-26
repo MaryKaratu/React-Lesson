6afabe6eb9564bc9fdfdf7c1b46666f53b651504
MaryKaratu <Karatumary85@gmail.com>
2025-11-26 16:02:48 +0100

    Mangaing State to Switch Between Components

--- code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevProject) => {
+      return {
+        ...prevProject,
+        selectedProject: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProject === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
